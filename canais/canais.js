44e0f579bcac9d61d91a4ee406f58975ff05e062
mikael-souza <contato@mikaelsouza.com.br>
2020-12-19 17:49:10 -0300

    Add files via upload

---
+++ canais/canais.js
@@ -0,0 +1,93 @@
+function load_space() {
+    document.getElementById('iframe1').src="https://megafilmeshd50.top/canais/deko.php?canal=space";
+    document.getElementById('iframe2').src="about:blank";
+    document.getElementById('iframe3').src="about:blank";
+    document.getElementById('iframe4').src="about:blank";
+    document.getElementById('iframe5').src="about:blank";
+    document.getElementById('iframe6').src="about:blank";
+    document.getElementById('iframe7').src="about:blank";
+    document.getElementById('iframe8').src="about:blank";
+    document.getElementById('iframe9').src="about:blank";
+    document.getElementById('iframe10').src="about:blank";
+    document.getElementById('iframe11').src="about:blank";
+    document.getElementById('iframe12').src="about:blank";
+    document.getElementById('iframe13').src="about:blank";
+    document.getElementById('iframe14').src="about:blank";
+    document.getElementById('iframe15').src="about:blank";
+    document.getElementById('iframe16').src="about:blank";
+    document.getElementById('iframe17').src="about:blank";
+    document.getElementById('iframe18').src="about:blank";
+    document.getElementById('iframe19').src="about:blank";
+    document.getElementById('iframe20').src="about:blank";
+
+} space.onclick = load_space;
+
+function load_universal() {
+    document.getElementById('iframe1').src="about:blank";
+    document.getElementById('iframe2').src="https://megafilmeshd50.top/canais/deko.php?canal=universal";
+    document.getElementById('iframe3').src="about:blank";
+    document.getElementById('iframe4').src="about:blank";
+    document.getElementById('iframe5').src="about:blank";
+    document.getElementById('iframe6').src="about:blank";
+    document.getElementById('iframe7').src="about:blank";
+    document.getElementById('iframe8').src="about:blank";
+    document.getElementById('iframe9').src="about:blank";
+    document.getElementById('iframe10').src="about:blank";
+    document.getElementById('iframe11').src="about:blank";
+    document.getElementById('iframe12').src="about:blank";
+    document.getElementById('iframe13').src="about:blank";
+    document.getElementById('iframe14').src="about:blank";
+    document.getElementById('iframe15').src="about:blank";
+    document.getElementById('iframe16').src="about:blank";
+    document.getElementById('iframe17').src="about:blank";
+    document.getElementById('iframe18').src="about:blank";
+    document.getElementById('iframe19').src="about:blank";
+    document.getElementById('iframe20').src="about:blank";
+} universal.onclick = load_universal;
+
+function load_mtv() {
+    document.getElementById('iframe1').src="about:blank";
+    document.getElementById('iframe2').src="about:blank";
+    document.getElementById('iframe3').src="https://megafilmeshd50.top/canais/deko.php?canal=mtv";
+    document.getElementById('iframe4').src="about:blank";
+    document.getElementById('iframe5').src="about:blank";
+    document.getElementById('iframe6').src="about:blank";
+    document.getElementById('iframe7').src="about:blank";
+    document.getElementById('iframe8').src="about:blank";
+    document.getElementById('iframe9').src="about:blank";
+    document.getElementById('iframe10').src="about:blank";
+    document.getElementById('iframe11').src="about:blank";
+    document.getElementById('iframe12').src="about:blank";
+    document.getElementById('iframe13').src="about:blank";
+    document.getElementById('iframe14').src="about:blank";
+    document.getElementById('iframe15').src="about:blank";
+    document.getElementById('iframe16').src="about:blank";
+    document.getElementById('iframe17').src="about:blank";
+    document.getElementById('iframe18').src="about:blank";
+    document.getElementById('iframe19').src="about:blank";
+    document.getElementById('iframe20').src="about:blank";
+} mtv.onclick = load_mtv;
+
+
+function load_syfy() {
+    document.getElementById('iframe1').src="about:blank";
+    document.getElementById('iframe2').src="about:blank";
+    document.getElementById('iframe3').src="about:blank";
+    document.getElementById('iframe4').src="https://megafilmeshd50.top/canais/deko.php?canal=syfy";
+    document.getElementById('iframe5').src="about:blank";
+    document.getElementById('iframe6').src="about:blank";
+    document.getElementById('iframe7').src="about:blank";
+    document.getElementById('iframe8').src="about:blank";
+    document.getElementById('iframe9').src="about:blank";
+    document.getElementById('iframe10').src="about:blank";
+    document.getElementById('iframe11').src="about:blank";
+    document.getElementById('iframe12').src="about:blank";
+    document.getElementById('iframe13').src="about:blank";
+    document.getElementById('iframe14').src="about:blank";
+    document.getElementById('iframe15').src="about:blank";
+    document.getElementById('iframe16').src="about:blank";
+    document.getElementById('iframe17').src="about:blank";
+    document.getElementById('iframe18').src="about:blank";
+    document.getElementById('iframe19').src="about:blank";
+    document.getElementById('iframe20').src="about:blank";
+} syfy.onclick = load_syfy;
