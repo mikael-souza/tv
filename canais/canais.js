1fdd26b6f10f53ae131bbad6e44b435c2d882de2
mikael-souza <contato@mikaelsouza.com.br>
2020-12-19 16:35:01 -0300

    Add files via upload

---
+++ canais/canais.js
@@ -0,0 +1,16 @@
+function load_space() {
+    document.getElementById('iframeid').src="https://megafilmeshd50.top/canais/deko.php?canal=space";
+} space.onclick = load_space;
+
+function load_universal() {
+    document.getElementById('iframeid').src="https://megafilmeshd50.top/canais/deko.php?canal=universal";
+} universal.onclick = load_universal;
+
+function load_mtv() {
+    document.getElementById('iframeid').src="https://megafilmeshd50.top/canais/deko.php?canal=mtv";
+} mtv.onclick = load_mtv;
+
+
+function load_syfy() {
+    document.getElementById('iframeid').src="https://megafilmeshd50.top/canais/deko.php?canal=syfy";
+} syfy.onclick = load_syfy;
